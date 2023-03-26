a68224f87abd612880bd6fdecbcd5c4acdc04cd1
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-03-26 04:02:06 +0200

    Create OutputWritterTest09.java

---
+++ TestCode/OutputWritterTest09.java
@@ -0,0 +1,39 @@
+import Output.OutputWritter;
+import Output.Student;
+import Output.Subject;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
+class OutputWritterTest {
+	String a,b,c,actual,save ="";
+	Scanner x;
+	@Test
+	void test() {
+		String expected = "Subject Name: Math Max Mark: 100 "
+				+ "Student name: MONA Student number: 1900150 GPA: 3.7 Grade: A- ";
+		ArrayList<Student> students = new ArrayList<>();
+		students.add(new Student("MONA", "1900150",6,10,17,58));
+		Subject s = new Subject("Math","12",100,students);
+		File file = new File("To.txt");
+		try {
+			OutputWritter.WriteFile(s, file);
+		}catch(IOException e) {
+				e.printStackTrace();
+		}
+		try {
+			x = new Scanner(new File("To.txt"));
+			while(x.hasNext()) {
+				a = x.next();
+				b = x.next();
+				actual = a + " " + b + " ";
+				save += actual;
+			}
+			assertEquals(expected, save, "OutputWritter::Test Failed");
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
