3dda9d6745f987c6fa53d36b8ebef8b9d44bf6d7
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-05-06 15:28:37 +0300

    Feat: constant strings

--- Constants.java
+++ Constants.java
@@ -1,5 +1,21 @@
-public class Constants
-{ 
+public class Constants {
     public static final String FULL_MARK_EXCEPTION_MSG = "full mark should be from 0 to 100!";
+    public static final String EM_STRING = "Empty file is not allowed";
+    public static final String invalidfile = "Invalid File extension ";
+    public static final String STUDE_DUP_STRING = "Student Id duplicate between -> x";
+    public static final String INVALIDLINE1_STRING = "Invalid File: Line 1 error";
+    public static final String EMPTY_LINE_STRING = "Empty record is not allowed";
+    public static final String ALPHA_ONLY_STRING = "Name of subject must contain Alphabetic only ";
+    public static final String LAST_LETTER_STRING = "Last digit mush contain only (s) (if exist)";
+    public static final String Invalid_Record_Subject_STRING = "Subject-code: It must be 6-7 Alphanumeric characters. The first 3 are Alphabetic\n"
+            + "followed by 3 numeric characters. The sevens should be s if exists.";
+    public static final String SPACE_STRING = "Can't contain space in the first";
+    public static final String EmptyField_STRING = "Can't provide empty ";
+    public static final String Student_LINE_STRING = "Invalid Line of students";
+    public static final String Student_LINE_Name_STRING = "Name of Student must contain Alphabetic only at line:";
+    public static final String Student_LINE_Code_STRING = "Student code :must contain numbers only Line: ";
+    public static final String Student_LINE_Activites_oral_STRING = "Student code :must contain numbers only Line: ";
+    public static final String Student_LINE_Mid_STRING = "Midterm must be in range (0-20) Integer Line :";
+    public static final String Student_LINE_Final_STRING = "FinalExam must be in range (0-60) Integer Line :";
 
 }
