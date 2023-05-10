59a56f58e5457a973e03806dde25c8ea494673bc
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2023-05-10 21:27:54 +0300

    added black box testing

--- FileAnalyzerNewTest.java
+++ FileAnalyzerNewTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
+/*
+ * BB in the function name stands for Black Box
+ * WB in the function name stands for White box 
+ */
 public class FileAnalyzerNewTest {
     FileAnalyzer fa ;
     String input ; 
@@ -21,86 +24,171 @@ public class FileAnalyzerNewTest {
         fa = null;
         input = null ;
     }
-    //testing for checkAlphabetic
+    //White box testing for checkAlphabetic
     @Test
-    public void testCheckAlphabetic01() {
-        input = "" ;
-        assertFalse("error in test case 1", FileAnalyzer.checkStringAlphabetic(input));
+    public void testCheckAlphabeticWB01() {
+        input = "                         " ;
+        assertFalse("error in white box test case 1 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic02() {
+    public void testCheckAlphabeticWB02() {
         input = "U" ;
-        assertTrue("error in test case 2", FileAnalyzer.checkStringAlphabetic(input));
+        assertTrue("error in white box test case 2 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic03() {
+    public void testCheckAlphabeticWB03() {
         input = "123" ;
-        assertFalse("error in test case 3", FileAnalyzer.checkStringAlphabetic(input));
+        assertFalse("error in white box test case 3 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic04() {
+    public void testCheckAlphabeticWB04() {
         input = "lorem ipsum dolor" ;
-        assertTrue("error in test case 4", FileAnalyzer.checkStringAlphabetic(input));
+        assertTrue("error in white box test case 4 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic05() {
+    public void testCheckAlphabeticWB05() {
         input = "mina$" ;
-        assertFalse("error in test case 5", FileAnalyzer.checkStringAlphabetic(input));
+        assertFalse("error in white box test case 5 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic06() {
+    public void testCheckAlphabeticWB06() {
         input = "mina_!mounir" ;
-        assertFalse("error in test case 6", FileAnalyzer.checkStringAlphabetic(input));
+        assertFalse("error in white box test case 6 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic07() {
+    public void testCheckAlphabeticWB07() {
         input = "min%1UMoun~R" ;
-        assertFalse("error in test case 7", FileAnalyzer.checkStringAlphabetic(input));
+        assertFalse("error in white box test case 7 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
     @Test
-    public void testCheckAlphabetic08() {
+    public void testCheckAlphabeticWB08() {
         input = "mInAMounir" ;
-        assertTrue("error in test case 8", FileAnalyzer.checkStringAlphabetic(input));
+        assertTrue("error in white box test case 8 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
     }
-    //testing for CheckAlphanumeric
+    //Black box testing for checkAlphabetic
     @Test
-    public void testCheckAlphaNumeric01() {
+    public void testCheckAlphabeticBB01() {
+        input = "Mina875Mounir@%F&rid" ;
+        assertFalse("error in black box test case 1 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB02() {
+        input = "Mina420Mounir Farid10" ;
+        assertFalse("error in black box test case 2 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB03() {
+        input = "/Mar^(naMila~d " ;
+        assertFalse("error in black box test case 3 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB04() {
+        input = "Ghaith Bassam Zaza" ;
+        assertTrue("error in black box test case 4 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB05() {
+        input = "(10+2)*3+[75]!" ;
+        assertFalse("error in black box test case 5 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB06() {
+        input = "1976435034567" ;
+        assertFalse("error in black box test case 6 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB07() {
+        input = "*/-+=`" ;
+        assertFalse("error in black box test case 7 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+    @Test
+    public void testCheckAlphabeticBB08() {
+        input = "                 " ;
+        assertFalse("error in black box test case 8 for CheckStringAlphabetic", FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    //white box  for CheckAlphanumeric
+    @Test
+    public void testCheckAlphaNumericWB01() {
         input = "" ;
-        assertFalse("error in test case 1", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 1 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     }
     @Test
-    public void testCheckAlphaNumeric02() {
+    public void testCheckAlphaNumericWB02() {
         input = "123" ;
-        assertFalse("error in test case 2", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 2 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     } 
      @Test
-    public void testCheckAlphaNumeric03() {
+    public void testCheckAlphaNumericWB03() {
         input = "U" ;
-        assertFalse("error in test case 3", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 3 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     } 
      @Test
-    public void testCheckAlphaNumeric04() {
+    public void testCheckAlphaNumericWB04() {
         input = "lorem ipsum dolor" ;
-        assertFalse("error in test case 4", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 4 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     } 
      @Test
-    public void testCheckAlphaNumeric05() {
+    public void testCheckAlphaNumericWB05() {
         input = "mina$" ;
-        assertFalse("error in test case 5", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 5 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     } 
      @Test
-    public void testCheckAlphaNumeric06() {
+    public void testCheckAlphaNumericWB06() {
         input = "mina_!mouni12r" ;
-        assertFalse("error in test case 6", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 6 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     } 
      @Test
-    public void testCheckAlphaNumeric07() {
+    public void testCheckAlphaNumericWB07() {
         input = "min%UMoun~R" ;
-        assertFalse("error in test case 7", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertFalse("error in white box test case 7 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     }
     @Test
-    public void testCheckAlphaNumeric08() {
+    public void testCheckAlphaNumericWB08() {
         input = "0mInAMou4nir4" ;
-        assertTrue("error in test case 8", FileAnalyzer.checkStringAlphaNumeric(input));
+        assertTrue("error in white box test case 8 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
     }
+    //black box  for CheckAlphanumeric
+    @Test
+    public void testCheckAlphaNumericBB01() {
+        input = "a[5] = a[4] + a[j++] ; " ;
+        assertFalse("error in black box test case 1 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB02() {
+        input = "Soft4ware Tes2ting100100" ;
+        assertTrue("error in black box test case 2 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB03() {
+        input = "if( a[I,j]) == B[r,C] then x += y " ;
+        assertFalse("error in black box test case 3 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB04() {
+        input = "Philopateer Moheb" ;
+        assertFalse("error in black box test case 4 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB05() {
+        input = "5! = (5+1)! / (5+1) = 120" ;
+        assertFalse("error in black box test case 5 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB06() {
+        input = "9873625014" ;
+        assertFalse("error in black box test case 6 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB07() {
+        input = "!^&*_+-*/" ;
+        assertFalse("error in black box test case 7 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+    @Test
+    public void testCheckAlphaNumericBB08() {
+        input = "" ;
+        assertFalse("error in black box test case 8 for CheckStringAlphaNumeric", FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+
+
 }
