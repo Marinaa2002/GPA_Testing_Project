9027e2ba4c39939ce27fd76b10e6334a3e950b03
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2023-05-15 19:48:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -8,20 +9,29 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Scanner s = new Scanner(System.in);
-        try {
-            
-            System.out.println("insert input file directory:");
-            File input = new File(s.nextLine());
-            Subject subject = FileAnalyzer.ReadFile(input);
-            System.out.println("insert output directory:");
-            File output = new File(s.nextLine());
-            OutputWritter.WriteFile(subject, output);
-            System.out.println("done.");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        while (true) {
+            try {
+                System.out.println("insert input file directory:");
+                File input = new File(s.nextLine());
+                Subject subject = FileAnalyzer.ReadFile(input);
+                System.out.println("insert output directory:");
+                File output = new File(s.nextLine());
+                OutputWritter.WriteFile(subject, output);
+                System.out.println("done.");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("To continue enter Y program will terminate otherwise:");
+            String cont = s.nextLine();
+            if(!cont.equalsIgnoreCase("y")){
+                break;
+            }
         }
-        s.nextLine();
+        System.out.println("Press Enter to exit..");
+        System.in.read();
     }
+
 }
