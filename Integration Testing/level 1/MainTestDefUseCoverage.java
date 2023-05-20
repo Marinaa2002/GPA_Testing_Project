7c14765e59a77ac2dfaba0fab616b0da24af71a5
Ghaith-Bassam-Zaza <104207733+Ghaith-Bassam-Zaza@users.noreply.github.com>
2023-05-20 09:12:48 +0300

    Create MainTestDefUseCoverage.java

---
+++ Integration Testing/level 1/MainTestDefUseCoverage.java
@@ -0,0 +1,88 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Ghaith
+ */
+public class MainTestDefUseCoverage {
+
+    public MainTestDefUseCoverage() {
+    }
+    
+    
+    @Test
+    public void testMainDefUse1() throws Exception {
+        String userInput = String.format("test cases/Fa_TC01.txt%sMainBaseCoverageTest1Out.txt%sN",
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+
+        String[] expectedLines = {Constants.INPUT_MASSAGE, Constants.OUTPUT_MASSAGE, Constants.SUCCESS_MASSAGE, Constants.RESTART_MASSAGE, Constants.EXIT_MASSAGE};
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        Main.main(null); // call the main method
+
+        String[] actualLines = baos.toString().split(System.lineSeparator());
+
+        for (int i = 0; i < expectedLines.length; i++) {
+            assertEquals(expectedLines[i], actualLines[i]);
+        }
+
+    }
+    
+    @Test
+    public void testMainDefUse2() throws Exception {
+        String userInput = String.format("test cases/invalid.txt%sMainBaseCoverageTest2Out.txt%sN",
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+
+        String[] expectedLines = {Constants.INPUT_MASSAGE, Constants.EM_STRING, Constants.RESTART_MASSAGE, Constants.EXIT_MASSAGE};
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        Main.main(null); // call the main method
+
+        String[] actualLines = baos.toString().split(System.lineSeparator());
+
+        for (int i = 0; i < expectedLines.length; i++) {
+            assertEquals(expectedLines[i], actualLines[i]);
+        }
+
+    }
+    
+    @Test
+    public void testMainDefUse3() throws Exception {
+        String out = "MainBaseCoverageTest3Out.txt";  // ** important ** : make sure to have MainBaseCoverageTest3Out.txt as read only file before running the test case
+        String userInput = String.format("test cases/Fa_TC01.txt%s"+out+"%sN", 
+                System.lineSeparator(),
+                System.lineSeparator());
+        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(bais);
+
+        String[] expectedLines = {Constants.INPUT_MASSAGE, Constants.OUTPUT_MASSAGE, out + " (Access is denied)", Constants.RESTART_MASSAGE, Constants.EXIT_MASSAGE};
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        Main.main(null); // call the main method
+
+        String[] actualLines = baos.toString().split(System.lineSeparator());
+
+        for (int i = 0; i < expectedLines.length; i++) {
+            assertEquals(expectedLines[i], actualLines[i]);
+        }
+
+    }
+    
+
+}
