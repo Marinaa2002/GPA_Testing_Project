63a8115a429151a4cf35de871662936581680ae0
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2023-05-20 16:04:27 +0300

    Fix: Repo

---
+++ Integration Testing/Level 5/CheckStringAlphabeticTest/CheckStringAlphabeticTestBB.java
@@ -0,0 +1,83 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import source.FileAnalyzer;
+
+//this class is concerned with testing CheckStringAlphabetic() function with black box testing technique
+
+public class CheckStringAlphabeticTestBB {
+    FileAnalyzer fa;
+    String input;
+
+    @BeforeEach
+    public void setUp() {
+        fa = new FileAnalyzer();
+        input = null;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        fa = null;
+        input = null;
+    }
+  
+
+    @Test
+    public void tc1() {
+        input = "Mina875Mounir@%F&rid";
+        assertFalse("error in black box test case 1 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB02() {
+        input = "Mina420Mounir Farid10";
+        assertFalse("error in black box test case 2 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB03() {
+        input = "/Mar^(naMila~d ";
+        assertFalse("error in black box test case 3 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB04() {
+        input = "Ghaith Bassam Zaza";
+        assertTrue("error in black box test case 4 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB05() {
+        input = "(10+2)*3+[75]!";
+        assertFalse("error in black box test case 5 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB06() {
+        input = "1976435034567";
+        assertFalse("error in black box test case 6 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB07() {
+        input = "*/-+=`";
+        assertFalse("error in black box test case 7 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticBB08() {
+        input = "                 ";
+        assertFalse("error in black box test case 8 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+}
---
+++ Integration Testing/Level 5/CheckStringAlphabeticTest/CheckStringAlphabeticTestWB.java
@@ -0,0 +1,89 @@
+package source;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+//this class is concerned with testing CheckStringAlphabetic() function with white box testing technique
+
+public class CheckStringAlphabeticTestWB {
+    FileAnalyzer fa;
+    String input;
+
+    @BeforeEach
+    public void setUp() {
+        fa = new FileAnalyzer();
+        input = null;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        fa = null;
+        input = null;
+    }
+
+    // White box testing for checkAlphabetic
+    @Test
+    public void testCheckAlphabeticWB01() {
+        input = "                         ";
+        assertFalse("error in white box test case 1 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB02() {
+        input = "U";
+        assertTrue("error in white box test case 2 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB03() {
+        input = "123";
+        assertFalse("error in white box test case 3 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB04() {
+        input = "lorem ipsum dolor";
+        assertTrue("error in white box test case 4 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB05() {
+        input = "mina$";
+        assertFalse("error in white box test case 5 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB06() {
+        input = "mina_!mounir";
+        assertFalse("error in white box test case 6 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB07() {
+        input = "min%1UMoun~R";
+        assertFalse("error in white box test case 7 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB08() {
+        input = "mInAMounir";
+        assertTrue("error in white box test case 8 for CheckStringAlphabetic",
+                FileAnalyzer.checkStringAlphabetic(input));
+    }
+
+    @Test
+    public void testCheckAlphabeticWB09() {
+        input = "مينا منير";
+        assertFalse("error in white box test case 8 for CheckStringAlphaNumeric",
+                FileAnalyzer.checkStringAlphaNumeric(input));
+    }
+}
